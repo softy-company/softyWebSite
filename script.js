5abd2afbca63d30b42f1f25c28e1476a16ad1c41
softy-company <vkserfingazi@gmail.com>
2023-11-09 17:02:53 +0600

    dd

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 const link = document.querySelector(".link");
-// console.log(link.href);
-link.style.border = "1px solid #000"
-// console.log(link.href);
+const nav = document.querySelector(".header-nav");
+const button = document.querySelector(".blog button");
+const icon = document.querySelector("ion-icon");
+
+button.addEventListener("click", () => {
+  check();
+});
+
+function check() {
+  if (nav.style.display === "none" || nav.style.display === "") {
+    icon.style.animation = "none";
+    setTimeout(() => {
+      icon.style.animation = "icon 0.3s ease-in-out";
+    }, 1);
+    nav.style.display = "block";
+  } else if (nav.style.display === "block") {
+    nav.style.display = "none";
+    icon.innerHTML = `<ion-icon name="arrow-back-outline"></ion-icon>`;
+  }
+}
+icon.addEventListener("animationend", () => {
+    if (nav.style.display === "none" || nav.style.display === "") {
+      // Иконка после завершения анимации отображения
+      icon.innerHTML = `<ion-icon name="arrow-forward-outline"></ion-icon>`;
+    } else if (nav.style.display === "block") {
+      // Иконка после завершения анимации скрытия
+      icon.innerHTML = `<ion-icon name="arrow-back-outline"></ion-icon>`;
+    }
+  });
+// check()
